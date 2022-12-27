a44d30290345631f5d6401b4901f34bd65751edc
kamruzzamanripon <rkamruzzaman@gmail.com>
2022-12-28 04:47:02 +0600

    first commit

---
+++ src/hooks/useAuthCheck.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { userLoggedIn } from '../features/auth/authSlice';
+
+const useAuthCheck = () => {
+    const dispatch = useDispatch();
+    const [authChecked, setAthChecked] = useState(false);
+
+    useEffect(()=>{
+        const localAuth = localStorage?.getItem('auth');
+
+        if(localAuth){
+            const auth = JSON.parse(localAuth);
+            if(auth?.accessToken && auth?.user){
+                dispatch(userLoggedIn({
+                    accessToken: auth.accessToken,
+                    user: auth.user,
+                }))
+            }
+        }
+
+        setAthChecked(true);
+    },[dispatch, setAthChecked])
+
+    return authChecked;
+   
+};
+
+export default useAuthCheck;
